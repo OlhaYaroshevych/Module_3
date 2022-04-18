da5255b3d527a729b5a77c8909eafe023caac02f
Olha Yaroshevych <jaroshewytsch.olia@gmail.com>
2022-04-18 11:45:28 +0300

    Creating the custom module that outputs custom page

---
+++ web/modules/custom/zin/src/Controller/ZinController.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @return
+ * Contains \Drupal\zin\Controller\ZinController.
+ */
+
+namespace Drupal\zin\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Returns responses for zin routes.
+ */
+class ZinController extends ControllerBase {
+
+  /**
+   * Builds the response.
+   */
+  public function content() {
+    $element = array(
+      '#markup' => 'Hello! You can add here a photo of your cat.',
+    );
+    return $element;
+  }
+
+}
